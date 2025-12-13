21686b017108ea119a59d5104b482ecfae5f76e5
javarcoding <ravirajadsul9297@gmail.com>
2025-12-13 08:14:39 +0530

    Initialize React Vite frontend project

---
+++ Homecooked-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <h1>Homecooked – Smart Tiffin Service Platform</h1>
+    </div>
+  )
+}
+
+export default App
